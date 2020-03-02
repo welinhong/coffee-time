e632e6c782692219d8d2b828d7caff888bda8a56
Welin Hong <danbi@healingpaper.com>
2020-03-02 09:45:24 +0900

    짝꿍 리스트 복사 기능 추가

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -38,7 +38,11 @@
     <form-container>
       <template #label>짝꿍리스트</template>
       <template #value>
-        <span v-if="groups.length">총 {{ groups.length }} 짝꿍</span>
+        <span v-if="groups.length">
+          <div>총 {{ groups.length }} 짝꿍</div>
+          <h-button @click="copyToClipboard">복사하기</h-button>
+        </span>
+        <h-space :size="5" />
         <div v-for="{ memberList, _id } in groups" :key="_id">
           <h-tag v-for="member in memberList" :key="`${_id}_${member._id}`">
             {{ member.name }}
@@ -95,6 +99,33 @@ export default {
       if (status !== 200) return
 
       this.groups = data
+    },
+    copyToClipboard() {
+      const groupStr = this.groups.reduce((groupStr, group) => {
+        const str = group.memberList.map((member) => member.name).join(' & ')
+        return groupStr += `${str} \n`
+      }, '')
+      const el = document.createElement('textarea')
+      el.value = groupStr
+      el.setAttribute('readonly', '')
+      el.style.position = 'absolute'
+      el.style.left = '-9999px'
+      document.body.appendChild(el)
+      
+      const selected = document.getSelection().rangeCount > 0
+        ? document.getSelection().getRangeAt(0)
+        : false;      
+      el.select()
+      document.execCommand('copy')
+      document.body.removeChild(el);
+
+      if (selected) {                                 
+        document.getSelection().removeAllRanges();    
+        document.getSelection().addRange(selected);   
+        this.$toast({
+          title: '짝꿍 리스트가 복사되었습니다!'
+        })
+      }
     },
     goToList() {
       this.$router.push('/event')
